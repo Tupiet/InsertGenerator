e1545a514370708c5fb8ab37f70829617a7fa7c9
Tupiet <62516852+Tupiet@users.noreply.github.com>
2022-07-18 12:05:34 +0200

    Format correction

--- Server/index.js
+++ Server/index.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+})
 
 app.use(express.static(path.join(__dirname, '../Client')))
 
